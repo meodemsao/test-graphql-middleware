da804be1b6ff99c7c330d6d3c07c9abacbc185eb
Bằng Vũ Công <bangvc@vgroup.vn>
2018-08-13 09:33:51 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,73 @@
+const { GraphQLServer } = require('graphql-yoga')
+const { Prisma } = require('prisma-binding')
+
+const resolvers = {
+  Query: {
+    feed(parent, args, ctx, info) {
+      return ctx.db.query.posts({ where: { isPublished: true } }, info)
+    },
+    drafts(parent, args, ctx, info) {
+      return ctx.db.query.posts({ where: { isPublished: false } }, info)
+    },
+    post(parent, { id }, ctx, info) {
+      return ctx.db.query.post({ where: { id } }, info)
+    },
+  },
+  Mutation: {
+    createDraft(parent, { title, text }, ctx, info) {
+      return ctx.db.mutation.createPost(
+        {
+          data: {
+            title,
+            text,
+          },
+        },
+        info,
+      )
+    },
+    deletePost(parent, { id }, ctx, info) {
+      return ctx.db.mutation.deletePost({ where: { id } }, info)
+    },
+    publish(parent, { id }, ctx, info) {
+      return ctx.db.mutation.updatePost(
+        {
+          where: { id },
+          data: { isPublished: true },
+        },
+        info,
+      )
+    },
+  },
+}
+
+const logMiddleware = async (resolve, parent, args, ctx, info) => {
+  try {
+    console.log(
+      `1 -> logInputMiddleware - Input arguments:  ${JSON.stringify(args)}`
+    )
+    const res = await resolve(parent, args, ctx, info)
+    console.log(`5 -> logInputMiddleware`)
+    return res
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+const middlewares = [logMiddleware]
+
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers,
+  middlewares: middlewares,
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      typeDefs: 'src/generated/prisma.graphql', // the auto-generated GraphQL schema of the Prisma API
+      endpoint: 'https://us1.prisma.sh/public-psychadelicsnagglefoot-466/my-app-test/dev', // the endpoint of the Prisma API
+      debug: true, // log all GraphQL queries & mutations sent to the Prisma API
+      // secret: 'mysecret123', // only needed if specified in `database/prisma.yml`
+    }),
+  }),
+})
+
+server.start(() => console.log('Server is running on http://localhost:4000'))
